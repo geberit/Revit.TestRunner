51ee60c20c55ff749e8ecd4de5112e1511409133
Tobias Flöscher <tobias.floescher@geberit.com>
2021-02-02 07:30:35 +0100

    Update Log4Net, NUnit

--- src/AssemblyInfo/AssemblyInfo.Version.cs
+++ src/AssemblyInfo/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion( "1.2.0.3" )]
-[assembly: AssemblyFileVersion( "1.2.0.3" )]
+[assembly: AssemblyVersion( "1.2.0.4" )]
+[assembly: AssemblyFileVersion( "1.2.0.4" )]
 
-[assembly: AssemblyInformationalVersion( "1.2.0.3" )]
+[assembly: AssemblyInformationalVersion( "1.2.0.4" )]
