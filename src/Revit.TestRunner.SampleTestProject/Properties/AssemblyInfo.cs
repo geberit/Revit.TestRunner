4ca44a400827b7a85c04e9c24b778507debdd39e
Tobias Flöscher <tobias.floescher@geberit.com>
2021-08-24 11:08:24 +0200

    Fix build issues

--- src/Revit.TestRunner.SampleTestProject/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle( "Revit.TestRunner.SampleTestProject" )]
-
-[assembly: ComVisible( false )]
-[assembly: Guid( "02702b89-16ea-4cc1-85f4-6cd7e2790dec" )]
-
---
+++ src/Revit.TestRunner.SampleTestProject2/ParameterTest.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI;
+using NUnit.Framework;
+
+namespace Revit.TestRunner.SampleTestProject
+{
+    public class ParameterTest
+    {
+        [SetUp]
+        public void SetUp( UIApplication uiApplication )
+        {
+            Assert.NotNull( uiApplication );
+        }
+
+        [TearDown]
+        public void TearDown( Application application )
+        {
+            Assert.NotNull( application );
+        }
+
+
+        [Test]
+        public void UiApplicationTest( UIApplication uiApplication )
+        {
+            Assert.NotNull( uiApplication );
+        }
+
+        [Test]
+        public void ApplicationTest( Application application )
+        {
+            Assert.IsNotNull( application );
+        }
+
+        [Test]
+        public void MultiParameterTest1( UIApplication uiApplication, Application application )
+        {
+            Assert.IsNotNull( uiApplication.Application );
+            Assert.IsNotNull( application );
+        }
+
+        [Test]
+        public void MultiParameterTest2( Application application, UIApplication uiApplication )
+        {
+            Assert.IsNotNull( uiApplication );
+            Assert.IsNotNull( application );
+        }
+
+        [TestCase( 12, 3, ExpectedResult = 15 )]
+        [TestCase( 13, 7, ExpectedResult = 20 )]
+        [TestCase( 15, 4, ExpectedResult = 19 )]
+        public int SumTest( int n, int d )
+        {
+            return n + d;
+        }
+    }
+}
