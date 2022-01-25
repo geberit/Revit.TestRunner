653c944f896e367bde4b34c3457c9aa232908082
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-01-24 21:32:55 -0300

    Fix NewRevit Not Close Add Wait ResultFile Add cmd Files Clear Project bin

--- src/Revit.TestRunner.Shared/RevitHelper.cs
+++ src/Revit.TestRunner.Shared/RevitHelper.cs
@@ -17,15 +17,15 @@ namespace Revit.TestRunner.Shared
         public static IEnumerable<string> GetInstalledRevitApplications()
         {
             var installedPrograms = GetInstalledApplications();
-            var revitInstallers = installedPrograms.Where( s => s.Contains( Revit ) );
+            var revitInstallers = installedPrograms.Where(s => s.Contains(Revit));
             var revitInstalled = revitInstallers
-                .Where( s => int.TryParse( s.Substring( s.Length - 4 ), out int i ) )
-                .Where( s => s.StartsWith( Revit ) )
-                .Where( s => s.Length == Revit.Length + 5 )
+                .Where(s => int.TryParse(s.Substring(s.Length - 4), out int i))
+                .Where(s => s.StartsWith(Revit))
+                .Where(s => s.Length == Revit.Length + 5)
                 .Distinct();
 
 
-            return revitInstalled.OrderBy( s => s );
+            return revitInstalled.OrderBy(s => s);
         }
 
         /// <summary>
@@ -36,17 +36,23 @@ namespace Revit.TestRunner.Shared
             var result = new List<string>();
             const string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
 
-            using( RegistryKey rk = Registry.LocalMachine.OpenSubKey( uninstallKey ) ) {
-                foreach( string skName in rk.GetSubKeyNames() ) {
-                    using( RegistryKey sk = rk.OpenSubKey( skName ) ) {
-                        try {
-                            var obj = sk.GetValue( "DisplayName" );
+            using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey))
+            {
+                foreach (string skName in rk.GetSubKeyNames())
+                {
+                    using (RegistryKey sk = rk.OpenSubKey(skName))
+                    {
+                        try
+                        {
+                            var obj = sk.GetValue("DisplayName");
 
-                            if( obj != null ) {
-                                result.Add( obj.ToString() );
+                            if (obj != null)
+                            {
+                                result.Add(obj.ToString());
                             }
                         }
-                        catch( Exception ) {
+                        catch (Exception)
+                        {
                             // ignored
                         }
                     }
@@ -61,41 +67,47 @@ namespace Revit.TestRunner.Shared
         /// If any Revit already run, no more Revit will be started.
         /// Returns the process id and if it was new started.
         /// </summary>
-        public static (int ProcessId, bool IsNew) StartRevit( string version, string language = "" )
+        public static (int ProcessId, bool IsNew) StartRevit(string version, string language = "")
         {
             Process process = null;
             bool isNew = false;
 
-            var processes = Process.GetProcessesByName( "Revit" );
+            var processes = Process.GetProcessesByName("Revit");
 
-            if( processes.Length == 0 ) {
-                var programFiles = Environment.GetFolderPath( Environment.SpecialFolder.ProgramFiles );
-                var executablePath = Path.Combine( programFiles, "Autodesk", $"Revit {version}", "Revit.exe" );
+            if (processes.Length == 0)
+            {
+                var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                var executablePath = Path.Combine(programFiles, "Autodesk", $"Revit {version}", "Revit.exe");
 
-                var argument = !string.IsNullOrEmpty( language )
+                var argument = !string.IsNullOrEmpty(language)
                     ? $" /language {language}"
                     : string.Empty;
 
-                if( File.Exists( executablePath ) ) {
-                    Console.WriteLine( $"Starting Revit {version}" );
+                if (File.Exists(executablePath))
+                {
+                    Console.WriteLine($"Starting Revit {version}");
 
-                    process = Process.Start( executablePath, argument );
+                    process = Process.Start(executablePath, argument);
                     isNew = true;
                 }
             }
-            else if( processes.Length == 1 ) {
+            else if (processes.Length == 1)
+            {
                 process = processes.Single();
+                Console.WriteLine($"Using Revit [{process.Id}]");
             }
-            else {
-                throw new ApplicationException( "Too many Revit applications already running! Max 1 allowed." );
+            else
+            {
+                throw new ApplicationException("Too many Revit applications already running! Max 1 allowed.");
             }
 
             return (process.Id, isNew);
         }
 
-        public static void KillRevit( int processId )
+        public static void KillRevit(int processId)
         {
-            var process = Process.GetProcessById( processId );
+            Console.WriteLine($"Killing Revit");
+            var process = Process.GetProcessById(processId);
             process?.Kill();
         }
     }
