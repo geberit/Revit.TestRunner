a87fbc5d76b361ea7db37f225a949086604db989
Tobias Flöscher <tobias.floescher@geberit.com>
2023-04-27 13:29:02 +0200

    Revit 2024

--- src/AssemblyInfo/AssemblyInfo.Version.cs
+++ src/AssemblyInfo/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion( "1.3.8.1" )]
-[assembly: AssemblyFileVersion( "1.3.8.1" )]
+[assembly: AssemblyVersion( "1.4.0.0" )]
+[assembly: AssemblyFileVersion( "1.4.0.0" )]
 
-[assembly: AssemblyInformationalVersion( "1.3.8.1" )]
+[assembly: AssemblyInformationalVersion( "1.4.0.0" )]
